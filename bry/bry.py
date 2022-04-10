5be6306f44957cd8725e1d3a0c289379e9251b4f
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-04-10 11:12:12 +0200

    .

--- bry/bry.py
+++ bry/bry.py
@@ -1,9 +1,12 @@
-from browser import document, html
-document <= html.H1("Hatványozás")
-for i in range(1,30):
-    document <= html.DIV(
-        html.SPAN(i) +
-        " négyzete: " +
-        html.SPAN(i ** 2)
-    )
-    print(i, i ** 2)
+from browser import document as D, html as H, ajax
+D <= H.H1("Brython alap")
+def f(x):
+    l = x.text.split("\n")
+    for r in l:
+        u = r.split(";")
+        D <= H.SPAN(u[1]) + ": " + H.SPAN(u[0]) + H.HR()
+def g(e):
+    D <= H.DIV(e.target.value)
+    e.target.value = ""
+D <= H.INPUT().bind("change", g) + H.HR()
+ajax.get("teszt.txt", oncomplete = f)
