b5634d2e7b659958d800746dd5fb9d3afbce2fff
Dr. Németh Tamás / Tamas Nemeth PhD <tomuwhu@gmail.com>
2022-04-28 18:03:02 +0200

    .

--- logic.py
+++ logic.py
@@ -1,9 +1,12 @@
 from browser import document as D, html as H
-D <= H.H1("Abszolút értékek")
+D <= H.H1("Táblázat")
 def f(x):
-    return x if x > 0 else -x 
+    return 1 if x == 1 else x * f(x-1)
 l = [
-    [f"{i}" for i in range(-5, 6)],
-    [f"{f(i)}" for i in range(-5, 6)]
+    [f"{i}<sup>2</sup>" for i in range(1, 10)],
+    [f"{i ** 2}" for i in range(1, 10)],
+    [""]*9,
+    [f"{i}!" for i in range(1, 10)],
+    [f"{f(i)}" for i in range(1, 10)]
 ]
 D <= H.TABLE(H.TR(H.TD(e) for e in r) for r in l)
