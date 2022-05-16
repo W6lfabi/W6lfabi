6c286ab6ae3b529020e0a7983895c71442894f4f
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-16 19:32:43 +0200

    .

--- bry/bry.py
+++ bry/bry.py
@@ -5,5 +5,5 @@ l  = list({R(1,10000) for i in range(200)})
 l  . sort()
 def f(e):
     e.target.innerHTML = ""
-    e.target.style.backgroundColor = "red"
+    e.target.style.backgroundColor = "green"
 D <= H.DIV([H.DIV(i).bind("click", f) for i in l],Class="c")
