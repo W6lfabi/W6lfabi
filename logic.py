b4a1f14074363cf9903a10062b20b476f327e4fd
Németh-Szabados Abigél <nemeth.szabados.abigel@gmail.com>
2022-05-16 19:36:11 +0200

    .

--- logic.py
+++ logic.py
@@ -1,12 +1,9 @@
 from browser import document as D, html as H
-D <= H.H1("Táblázat")
+from random import randrange as R
+r = [i for i in range(1,60)]
 def f(x):
-    return 1 if x == 1 else x * f(x-1)
-l = [
-    [f"{i}<sup>2</sup>" for i in range(1, 10)],
-    [f"{i ** 2}" for i in range(1, 10)],
-    [""]*9,
-    [f"{i}!" for i in range(1, 10)],
-    [f"{f(i)}" for i in range(1, 10)]
-]
-D <= H.TABLE(H.TR(H.TD(e) for e in r) for r in l)
+    return R(1, 60)
+r.sort(key = f)
+l = r[:20]
+l.sort()
+D <= H.DIV([H.DIV(f"{i} : {j}") for i,j in enumerate(l)], Class = "c")
