8c14d0e81fbc89a592463976707d315453d2d1ab
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-05-19 16:27:58 +0200

    .

--- logic.py
+++ logic.py
@@ -1,11 +1,19 @@
 from browser import document as D, html as H
-from random import shuffle
+from random import shuffle, randrange as R
+s = [1,0,1,2,3,2,1,0,1]
+def f(e):
+    e.target.text = int(e.target.text) + 1
+    e.target.style = {
+        "boxShadow": "1px 1px 6px black",
+        "backgroundColor": f"rgb({R(0,256)},{R(0,256)},{R(0,256)})"
+    }
 for i in range(6):
     l = list(range(1, 91))
     shuffle(l)
-    l = l[:5]
+    l = l[:9]
     l.sort()
     D <= [
-        H.DIV(e, Class=f"c{i % 3}" ) for i, e in enumerate(l)
+        H.DIV(e, Class=f"c{s[i]}").bind("click", f)
+        for i, e in enumerate(l)
     ]
     D <= H.HR()
