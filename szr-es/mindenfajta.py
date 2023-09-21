7e6c23f8946b6b74fe94858b6c262ab3c7f560d8
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-09-21 18:59:12 +0200

    .

--- szr-es/mindenfajta.py
+++ szr-es/mindenfajta.py
@@ -2,7 +2,6 @@ f = int(input("Forrás számrendszer (2 - 36): "))
 c = int(input("Cél számrendszer (2 - 36): "))
 kj = ["as", "es", "es", "as", "es", "ös", "os", "es", "as", "es"] * 5
 kj[10], kj[40] = "es", "es"
-az = " z   z" + " " * 40
 szj = [chr(ord("0") + i) for i in range(10)] + [chr(i) for i in range(65, 91)]
 szjo = {j: i for i, j in enumerate(szj)}
 n = input(f"Átváltandó szám (csak a 0-{szj[f-1]} számjegyek): ")
@@ -11,16 +10,20 @@ ns.reverse()
 o, l, k = 0, [1], 1
 for i, j in enumerate(ns):
     o += (f**i) * szjo[j]
-print(f"A{az[szjo[ns[-1]]]} „{n}” ({f}-{kj[f]} számrendszerbeli) szám")
-print(f"\t- tízes számrendszerbeli alakja: {o}")
+print(
+    f"A{'z' if ns[-1] in '15AEFILMNORSUXY' else ''} „{n}” ({f}-{kj[f]} számrendszerbeli) szám"
+)
+print(f"\t- tízes számrendszerbeli alakja: \N{ESC}[38;2;{255};{155};{200}m{o}\u001b[0m")
 if c != 10:
     while k <= o:
         k *= c
         l.append(k)
-    l.pop()
-    l.reverse()
-    print(f"\t- {c}-{kj[c]} számrendszerbeli alakja: ", end="")
+    l.pop(), l.reverse()
+    print(
+        f"\t- {c:2d}-{kj[c]} számrendszerbeli alakja: \N{ESC}[38;2;{255};{255};{100}m",
+        end="",
+    )
     for i in l:
         print(o // i, end="")
         o = o % i
-    print()
+    print("\u001b[0m")
